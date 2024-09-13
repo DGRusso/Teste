2865229b66d0b67a4c95bc14a2115031838174bd
DGRusso <glhrm.russo@gmail.com>
2024-09-13 15:46:17 +0000

    v2

--- FreeCodeCamp DataAnalysis/Mean-Variance-Standard Deviation Calculator/mean_var_std.py
+++ FreeCodeCamp DataAnalysis/Mean-Variance-Standard Deviation Calculator/mean_var_std.py
@@ -4,33 +4,30 @@ def calculate(nums):
     if len(nums) != 9:
         raise ValueError('List must contain nine numbers.')
     array = np.array(nums)
-    matrixnp = array.reshape((3, 3))
+    matrix = array.reshape((3, 3))
     print(matrix)
-    meanT = np.mean(matrix)
-    meanrow = np.mean(matrix, axis=1)
-    meancolumn = np.mean(matrix, axis=0)
-    varT = np.var(matrix)
-    varcolumn = np.var(matrix, axis=0)
-    varrow = np.var(matrix, axis=1)
-    stdT = np.std(matrix)
-    stdcolumn = np.std(matrix, axis=0)
-    stdrow = np.std(matrix, axis=1)
-    maxT = np.max(matrix)
-    maxcolumn = np.max(matrix, axis=0)
-    maxrow = np.max(matrix, axis=1)
-    minT = np.min(matrix)
-    mincolumn = np.min(matrix, axis=0)
-    minrow = np.min(matrix, axis=1)
-    sumT = np.sum(matrix)
-    sumcolumn = np.sum(matrix, axis=0)
-    sumrow = np.sum(matrix, axis=1)
+    meanT = np.mean(matrix).tolist()
+    meanrow = np.mean(matrix, axis=1).tolist()
+    meancolumn = np.mean(matrix, axis=0).tolist()
+    varT = np.var(matrix).tolist()
+    varcolumn = np.var(matrix, axis=0).tolist()
+    varrow = np.var(matrix, axis=1).tolist()
+    stdT = np.std(matrix).tolist()
+    stdcolumn = np.std(matrix, axis=0).tolist()
+    stdrow = np.std(matrix, axis=1).tolist()
+    maxT = np.max(matrix).tolist()
+    maxcolumn = np.max(matrix, axis=0).tolist()
+    maxrow = np.max(matrix, axis=1).tolist()
+    minT = np.min(matrix).tolist()
+    mincolumn = np.min(matrix, axis=0).tolist()
+    minrow = np.min(matrix, axis=1).tolist()
+    sumT = np.sum(matrix).tolist()
+    sumcolumn = np.sum(matrix, axis=0).tolist()
+    sumrow = np.sum(matrix, axis=1).tolist()
     calculations =  {"mean": [meancolumn, meanrow, meanT], 
                     "variance": [varcolumn, varrow, varT],
                     "standard deviation": [stdcolumn, stdrow, stdT],
                     "max": [maxcolumn, maxrow, maxT],
                     "min": [mincolumn, minrow, minT],
                     "sum": [sumcolumn, sumrow, sumT]}
-
-
-
-calculate([0,1,2,3,4,5,6,7,8])
+    return calculations
